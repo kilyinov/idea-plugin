3341e7e052598939ce381322487686a8d224d06e
GitHub Action <action@github.com>
2021-09-17 04:08:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/kilyinov/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kilyinov/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kilyinov.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kilyinov.ideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kilyinov/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kilyinov/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kilyinov.ideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kilyinov.ideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
